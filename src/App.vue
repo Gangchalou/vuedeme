df3fd7b056151b264ebde265e0ac94b12ede8c1c
WenShuZhou <shuzhou468@sina.com>
2018-02-27 20:49:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>vue第一份项目</h2>
+    <img class="logo" src="./assets/logo.png" alt="logo">
+    <HelloWorld/>
+
+  </div>
+</template>
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    components:{
+      HelloWorld
+    }
+  }
+</script>
+<style>
+  .logo{
+    width: 200px;
+    height: 200px;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h2>{{msg}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg:'it is so cool'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  h2{
+    color: red;
+  }
+</style>
